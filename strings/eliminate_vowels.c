cf51bf4e3d87d493da07911167b81dd1468a20df
Sai Smruti Moharana <163166823+Sai-Smruti-M@users.noreply.github.com>
2024-10-24 08:59:44 +0530

    Add files via upload

---
+++ strings/eliminate_vowels.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,j=0;
+    printf("enter the length of the string=");
+    scanf("%d",&n);
+    getchar();
+    char *s,*wv;
+    s=(char*)calloc(n,sizeof(char));
+    wv=(char*)calloc(n,sizeof(char));
+    printf("enter the string:");
+    fgets(s,n+1,stdin);
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if(s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u'&&s[i]!='A'&&s[i]!='E'&&s[i]!='I'&&s[i]!='O'&&s[i]!='U')
+        {
+            wv[j++]=s[i];
+
+        }
+    }
+    puts(wv);
+    free(s);
+    free(wv);
+
+
+}
