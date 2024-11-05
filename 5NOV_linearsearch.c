97f1cd99565d2a6ee48fcfb6df5764cc1d7d7f72
Sai Smruti Moharana <163166823+Sai-Smruti-M@users.noreply.github.com>
2024-11-05 06:57:33 +0530

    Update 5NOV_linearsearch.c

---
+++ 5NOV_linearsearch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, search;
+    printf("Enter the length of the array=");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the number to search=");
+    scanf("%d", &search);
+
+    int i, found = 0;
+    for (i = 0; i < n; i++) {
+        if (a[i] == search) {
+            printf("The number %d is found at position %d\n", search, i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("The number %d is not found in the array.\n", search);
+    }
+
+    return 0;
+}
